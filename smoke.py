16f6a845e9dd15d4472a26be908d4e7e7ba8c130
jeangoujan <104026427+jeangoujan@users.noreply.github.com>
2024-05-16 18:03:56 +0500

    fix the gemini bug

--- smoke.py
+++ smoke.py
@@ -15,8 +15,8 @@ import logging
 apihelper.READ_TIMEOUT = 35
 apihelper.CONNECT_TIMEOUT = 35
 
-bot = telebot.TeleBot("6896373452:AAFgVQcGNIfaG09TCdGPeh_TLKsmnsptP2U")
-GEMINI_API_KEY = "AIzaSyBpknvRWrw0qtI7JRl3W9xUt9c9VFpnlkM"
+bot = telebot.TeleBot("6896373452:AAFgVQcGNIfaG09TCdGPeh_TLKsmnsptP2U") #
+GEMINI_API_KEY = "AIzaSyA0efxMtuOX-SI-tLZfsGrXh-cG16pV7Pc"
 
 ongoing_polls = {}
 
